5493a5e43e6b10272bf5dc76de19882c8b0cc68d
IVIosab <mosab.f.r@gmail.com>
2020-08-10 17:34:12 +0300

    ba da bim ba da boom

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+//#define f first
+//#define s second
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cout<<"TEST"<<endl;
+
+    return 0;
+}
